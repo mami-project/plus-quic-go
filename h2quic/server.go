0261eeff7a4b6589a41be053ada676f9270432d1
Roman Müntener <me@mroman.ch>
2017-10-24 17:07:24 +0200

    * false -> true

--- h2quic/server.go
+++ h2quic/server.go
@@ -86,7 +86,7 @@ func (s *Server) serveImpl(tlsConfig *tls.Config, conn *net.UDPConn) error {
 	config := quic.Config{
 		TLSConfig: tlsConfig,
 		Versions:  protocol.SupportedVersions,
-		UsePLUS: false,
+		UsePLUS: true,
 	}
 
 	var ln quic.Listener
