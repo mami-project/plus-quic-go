4e0a73c56f7ea5f745b8a36a58d3a73c489b8496
Roman Müntener <me@mroman.ch>
2018-06-06 11:29:07 +0200

    * collect data and send as JSON to a RESTful endpoint

--- example/puic-poll/README.md
+++ example/puic-poll/README.md
@@ -1,25 +1,17 @@
 # puic-poll
 
 puic-poll takes a `;` separated list of URLs and randomly polls one of them and then waits for a specified amount
-of seconds before polling. It writes statistics to a file. Each line in the statistics file has the format
-
-```
-unixtimestamp<TAB>(ERR | OK);size;speed;elapsed;status code;error message
-```
-
-Example:
-
-```
-$ cat puic-poll.csv 
-1528273755	OK;256;0.017063;0.014308;200;""
-1528273760	OK;256;0.017713;0.013783;200;""
-1528273765	OK;256;0.012648;0.019302;200;""
-```
+of seconds before polling. It writes statistics to a file as JSON and can send the statistics to a remote
+REST API. 
 
 ```
 Usage of ./puic-poll:
+  -collect int
+    	How many statistics items to collect before sending them. (default 10)
   -logfile string
     	File to write debug information to. (default "puic-poll.log")
+  -send-to string
+    	Where to send statistics to.
   -statsfile string
     	File to write statistics to. (default "puic-poll.csv")
   -urls string
@@ -27,5 +19,8 @@ Usage of ./puic-poll:
   -wait int
     	Time to wait in seconds before making the next request. (default 1)
 ```
+
+If `send-to` is specified puic-poll will collect `collect` stats items before sending them as a POST request as `application/json`
+to the URL provided in `send-to`. 
 
 
--- example/puic-poll/main.go
+++ example/puic-poll/main.go
@@ -11,6 +11,7 @@ import (
 	"bytes"
 	"strings"
 	"math/rand"
+	"encoding/json"
 
 	"github.com/lucas-clemente/quic-go/h2quic"
 )
@@ -27,11 +28,21 @@ func openAppend(path string) (*os.File, error) {
 
 func writeOrDie(dst io.Writer, msg string, args... interface{}) {
 	now := time.Now().Unix()
-	_, err := io.WriteString(dst, fmt.Sprintf("%d\t", now) + fmt.Sprintf(msg, args...))
+	_, err := io.WriteString(dst, fmt.Sprintf("%d\t", now) + fmt.Sprintf(msg, args...) + "\n")
 
 	if err != nil {
 		panic(err)
 	}
+}
+
+type Stats struct {
+	Success bool
+	Size int64
+	Speed float64
+	Elapsed float64
+	StatusCode int
+	Message string
+	Now int64
 }
 
 func main() {
@@ -39,12 +50,17 @@ func main() {
 	var logfilePath = flag.String("logfile", "puic-poll.log", "File to write debug information to.")
 	var statsfilePath = flag.String("statsfile", "puic-poll.csv", "File to write statistics to.")
 	var wait = flag.Int("wait", 1, "Time to wait in seconds before making the next request.")
+	var collect = flag.Int("collect", 10, "How many statistics items to collect before sending them.")
+	var sendto = flag.String("send-to", "", "Where to send statistics to.")
 
 	flag.Parse()
 
 	var logfile *os.File
 	var statsfile *os.File
 	var err error
+	
+	collectedStats := make([]Stats, *collect)
+	j := 0
 
 	if *logfilePath != "" {
 		logfile, err = openAppend(*logfilePath)
@@ -63,7 +79,7 @@ func main() {
 		statsfile, err = openAppend(*statsfilePath)
 
 		if err != nil {
-			writeOrDie(logfile, fmt.Sprintf("ERR: Could not open statsfile `%s': %q\n", statsfilePath, err.Error()))
+			writeOrDie(logfile, fmt.Sprintf("ERR: Could not open statsfile $q: %q", statsfilePath, err.Error()))
 			panic(err)
 		}
 
@@ -74,29 +90,81 @@ func main() {
 
 	urlsToFetch := strings.Split(*urls, ";")
 
+	var netClient = &http.Client{
+	  Timeout: time.Second * 5,
+	}
+
 	for {
 
 		urlToFetch := urlsToFetch[rand.Int() % len(urlsToFetch)]
 
 		size, speed, elapsed, statusCode, err := fetchOnce(urlToFetch, logfile)
 
-		var statsLine string
-
-		if err != nil {
-			writeOrDie(logfile, fmt.Sprintf("ERR: Could not fetch %q: %q\n", urlToFetch, err.Error()))
-			statsLine = fmt.Sprintf("ERR;%d;%f;%f;%d;%q\n", size, speed, elapsed, statusCode, err.Error())
-		} else {
-			statsLine = fmt.Sprintf("OK;%d;%f;%f;%d;%q\n", size, speed, elapsed, statusCode, "")
+		stats := Stats {
+			Size : size,
+			Speed : speed,
+			Elapsed : elapsed,
+			StatusCode : statusCode,
+			Now : time.Now().Unix(),
 		}
 
-		writeOrDie(statsfile, statsLine)
+		if err != nil {
+			stats.Success = false
+			stats.Message = err.Error()
+		} else {
+			stats.Success = true
+			stats.Message = ""
+		}
+
+		collectedStats[j] = stats
+
+		statbytes, err := json.Marshal(stats)
+
+		if err != nil {
+			writeOrDie(logfile, fmt.Sprintf("ERR: Error: %q", err.Error()))
+			panic(err)
+		}
+
+		statstr := string(statbytes)
+
+		writeOrDie(statsfile, statstr)
+
+		j++
+
+		fmt.Println(j,*collect,*sendto)
+
+		if j >= *collect {
+			sendbytes, err := json.Marshal(collectedStats)
+
+			if err != nil {
+				writeOrDie(logfile, fmt.Sprintf("ERR: Error: %q", err.Error()))
+				panic(err)
+			}
+
+			if *sendto != "" {
+				writeOrDie(logfile, "Attempting to send...")
+				sendTo(netClient, *sendto, sendbytes, logfile)
+			}
+
+			j = 0
+		}
 
 		time.Sleep(time.Duration(*wait) * time.Second)
 	}
 }
 
+func sendTo(client *http.Client, url string, data []byte, log io.Writer) {
+	_, err := client.Post(url, "application/json", bytes.NewBuffer(data))
+
+	if err != nil {
+		writeOrDie(log, fmt.Sprintf("ERR: Could not send data to %q: %q", url, err.Error()))
+	} else {
+		writeOrDie(log, "Sent to %q", url)
+	}
+}
+
 func fetchOnce(url string, log io.Writer) (int64, float64, float64, int, error) {
-	writeOrDie(log, "Start fetching %q\n", url)
+	writeOrDie(log, "Start fetching %q", url)
 
 	hclient := &http.Client{
 		Transport: &h2quic.QuicRoundTripper{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}},
@@ -110,8 +178,8 @@ func fetchOnce(url string, log io.Writer) (int64, float64, float64, int, error) 
 		return 0, 0.0, 0.0, -1, err
 	}
 
-	writeOrDie(log, "Status code: %d\n", rsp.StatusCode)
-	writeOrDie(log, "Content length: %d\n", rsp.ContentLength)
+	writeOrDie(log, "Status code: %d", rsp.StatusCode)
+	writeOrDie(log, "Content length: %d", rsp.ContentLength)
 
 	n, err := io.Copy(&bytes.Buffer{}, rsp.Body)
 
@@ -121,11 +189,11 @@ func fetchOnce(url string, log io.Writer) (int64, float64, float64, int, error) 
 	speed := float64(n) / elapsed
 	speed = speed / (1024.0 * 1024.0)
 
-	writeOrDie(log, "Speed: %f MiB/s, elapsed: %f seconds\n", speed, elapsed)
+	writeOrDie(log, "Speed: %f MiB/s, elapsed: %f seconds", speed, elapsed)
 
 	rsp.Body.Close()
 
-	writeOrDie(log, "Done fetching `%s'\n", url)
+	writeOrDie(log, "Done fetching %q", url)
 
 	return n, speed, elapsed, rsp.StatusCode, err
 }
